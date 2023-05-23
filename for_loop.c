e21e4df1067d70c6900764d11c977a2741facb85
root <root@DESKTOP-5J8PIAC>
2023-05-23 16:01:27 +0200

    for_loop.c

--- for_loop.c
+++ for_loop.c
@@ -3,7 +3,12 @@
 int main()
 {
 	char l[] = "Wenn Sie das hier lesen, müssen Sie 100\% für die Abgabe geben";
-	printf("%l\n", l);
+	int länge = sizeof(l) / sizeof(l[0]);
+
+	for (int i = 0; i < länge; i++)
+	{
+		printf("%l\n", l[i]);
+	}
 
 return 0;
 }
